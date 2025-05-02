20554ea90c34fb78bf58fa46d5b73a1b1e678375
Ayushkumarsinha123 <itsharshit0005@gmail.com>
2025-05-02 19:59:35 +0530

    Complete dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
      <div className='flex flex-col flex-1 relative'>
       <Navbar />
      
-     <div className='grid md: grid-cols-3 grid-cols-1 w.full'>
+     <div className='grid md: grid-cols-3 w.full'>
       
       <div className='col-span-2'><LeftColumn /></div>
       <div className='w-full'><RightColumn /></div>
